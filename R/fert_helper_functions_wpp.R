d2af11a574d6ce6edbb2a3051a253205f63dfaea
sarahertog <sarahertog@gmail.com>
2020-06-05 12:52:03 -0400

    Helper functions used for computing fertility indicators needed for WPP

---
+++ R/fert_helper_functions_wpp.R
@@ -0,0 +1,63 @@
+#' fertility computation helper functions for WPP
+#' 
+#' 
+#' Compute percentage age-specific fertility rates from age-specific fertility rates 
+#'
+#' @description This function computes the percentage age-specific fertility rates from the age-specific fertility
+#' rates of a given period.
+#'
+#' @author Sara Hertog
+#'
+#' @param fert_data_age data frame. column "age" is age group of mother and "value" is age-specific fertility rates 
+#' in births per woman.
+#' @param byvar character. string of column names in fert_data_age by which pasfr will be computed.
+#'
+#' @details age/period can be 1x1 or 5x5.
+#'
+#' @return a data frame with percentage age specific fertility rates in the "value" field, computed by byvar 
+#' columns and age. Sums to 100 for each combination of byvar.
+#' @export
+fert_pasfr               <- function(fert_data_age, byvar) {
+  
+  age_width              <- fert_data_age$age[2] - fert_data_age$age[1]
+  fert_rate_total        <- aggregate(fert_data_age$value * age_width,
+                                      by = list(fert_data_age[,c(byvar)]),
+                                      sum)
+  names(fert_rate_total) <- c(byvar, "tfr")
+  pasfr                  <- merge(fert_data_age, fert_rate_total, by=byvar)
+  pasfr$value            <- pasfr$value * age_width / pasfr$tfr * 100
+  pasfr                  <- pasfr[, c(byvar, "age", "value")]
+  return(pasfr)
+  
+}
+
+#' Compute mean age at childbearing (mac) from age-specific fertility rates 
+#'
+#' @description This function computes the mean age at childbearing from the age-specific fertility rates 
+#' of a given period.
+#'
+#' @author Sara Hertog
+#'
+#' @param fert_data_age data frame. column "age" is age group of mother and "value" is age-specific fertility rates in births per woman.
+#' @param byvar character. string of column names in fert_data_age by which pasfr will be computed.
+#'
+#' @details age/period can be 1x1 or 5x5.
+#'
+#' @return a data frame with mean age at childbearing in the "value" field, computed by byvar columns and age.
+#' @export
+fert_mac                  <- function(fert_data_age, byvar) {
+  
+  age_width               <- fert_data_age$age[2] - fert_data_age$age[1]
+  sum_fert_rate_times_age <- aggregate(fert_data_age$value * (fert_data_age$age + age_width/2),
+                                       by = list(fert_data_age[,c(byvar)]),
+                                       sum)
+  sum_fert_rate           <- aggregate(fert_data_age$value,
+                                       by = list(fert_data_age[,c(byvar)]),
+                                       sum)
+  value                   <- sum_fert_rate_times_age$x / sum_fert_rate$x
+  mac                     <- as.data.frame(cbind(sum_fert_rate[,1:(ncol(sum_fert_rate)-1)],
+                                                 value))
+  names(mac)              <- c(byvar, "value")
+  return(mac)
+  
+}

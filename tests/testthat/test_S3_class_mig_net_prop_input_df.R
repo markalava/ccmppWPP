9b6eae433ea7702fc79b4bb438a4696842d4d4cc
Mark Wheldon <wheldon@un.org>
2020-08-17 14:30:15 -0400

    Small addition to tests

--- tests/testthat/test_S3_class_mig_net_prop_input_df.R
+++ tests/testthat/test_S3_class_mig_net_prop_input_df.R
@@ -18,3 +18,16 @@ test_that("valid member created", {
 
 })
 
+test_that("subsetting works", {
+    x <- ccmpp_input_list_example
+    x <- mig_net_prop_age_sex(x)
+
+    y <- subset_sex(x, "female")
+    expect_s3_class(y, "mig_net_prop_age_sex")
+    expect_identical(demog_change_component_dimensions(y),
+                     c("time", "sex", "age"))
+
+    ## Cannot work because the class 'mig_net_prop_age_sex' has to have a 'sex' dimension.
+    expect_error(subset_sex(x, "female", drop = TRUE),
+                 "'x' must have columns 'time_start', 'sex', 'age_start', 'value'; some or all are missing")
+    })

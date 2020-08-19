18525a6c5cfb7ce80e0a829cd338475bdbabccc5
Mark Wheldon <wheldon@un.org>
2020-08-19 16:19:40 -0400

    `prepare_df_for_demog_change_component_df` now always coerces `x` to a data frame.
    - Coercion is via `as.data.frame`.
    - Previously, only `demog_change_component_df` objects were coerced but objects like `tibble`s pass as `data.frame`s but cause errors later in the function due to special methods for `[` (and possibly others).

--- tests/testthat/test_S3_methods_tidyverse.R
+++ tests/testthat/test_S3_methods_tidyverse.R
@@ -26,3 +26,11 @@ test_that("Classes and attributes are dropped by tidyr functions", {
                       age_start, sex, sep = "_")
     expect_not_s3_class(x, "demog_change_component_df")
     })
+
+test_that("tibbles are handled", {
+    if (!requireNamespace("tibble", quietly = TRUE))
+        skip("'tibble' not installed.")
+    expect_s3_class(demog_change_component_df(
+        tibble::as_tibble(S3_demog_change_component_time_age_sex_test_df)),
+        "demog_change_component_df")
+})

c8f0a164f610353e86ff93fc41a166736923521a
Mark Wheldon <wheldon@un.org>
2021-06-10 10:36:14 -0400

    Remove dependency on 'git2r' package from '__install.R'. Does not affect the package in any way.

--- __install.R
+++ __install.R
@@ -16,9 +16,6 @@
 ###-----------------------------------------------------------------------------
 ### * Setup
 
-library(git2r)
-library(devtools)
-
 ###-----------------------------------------------------------------------------
 ### * Functions
 
@@ -34,44 +31,6 @@ sourceDir <- function(path, trace = TRUE, ...) {
         source(file.path(path, nm), ...)
         if(trace) cat("\n")
     }
-}
-
-###-----------------------------------------------------------------------------
-### ** Github stuff
-
-write_sha1_DESC <- function(pkg_dir, git_dir) {
-    ## Create new line with SHA1
-    new_sha1_line <- paste0("GitHubSHA1LastCommit: ", git2r::sha(git2r::last_commit(git_dir)))
-
-    ## Read DESC file as lines
-    DESC_con <- file(file.path(pkg_dir, "DESCRIPTION"))
-    DESC_lines <- readLines(DESC_con)
-
-    ## Is there already a 'GitHubSHA1LastCommit:' line? If so, replace it. If
-    ## not, add one.
-    sha1_line_num <- grep("GitHubSHA1LastCommit:", DESC_lines)
-    if(identical(length(sha1_line_num), 1L)) DESC_lines[sha1_line_num] <- new_sha1_line
-    else DESC_lines <- rbind(DESC_lines, new_sha1_line)
-
-    ## Write DES
-    writeLines(DESC_lines, con = DESC_con)
-
-    close(DESC_con)
-}
-
-clean_sha1_DESC <- function(pkg_dir) {
-    ## Read DESC file as lines
-    DESC_con <- file(file.path(pkg_dir, "DESCRIPTION"))
-    DESC_lines <- readLines(DESC_con)
-
-    ## Is there already a 'GitHubSHA1LastCommit:' line? If so, replace it with 'GitHubSHA1LastCommit: -'.
-    sha1_line_num <- grep("GitHubSHA1LastCommit:", DESC_lines)
-    if(identical(length(sha1_line_num), 1L)) DESC_lines[sha1_line_num] <- "GitHubSHA1LastCommit: -"
-
-    ## Write DES
-    writeLines(DESC_lines, con = DESC_con)
-
-    close(DESC_con)
 }
 
 ###-----------------------------------------------------------------------------

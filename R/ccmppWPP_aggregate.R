65f486e1439694ee034a59e338dad4663c78ad19
sarahertog <sarahertog@gmail.com>
2022-04-05 15:38:41 -0400

    changed print to log_print()

--- R/ccmppWPP_aggregate.R
+++ R/ccmppWPP_aggregate.R
@@ -189,14 +189,14 @@ ccmppWPP_compile <- function(locationIDs, base_year, last_year, intermediate_out
     if (has_outputs) {
       load(paste0(intermediate_output_folder, locationIDs[i], "_ccmpp_output.RData")) # need to verify whether we changed ccmpp intermediates to rda
     } else {
-      print(paste0("Warning: No CCMPP outputs available for LocID = ", locationIDs[i],". This location is excluded from aggregate."))
+      log_print(paste0("Warning: No CCMPP outputs available for LocID = ", locationIDs[i],". This location is excluded from aggregate."))
       next()
     }
     
     # check whether base year agrees
     check_base_year <- min(ccmpp_output$pop_count_age_sex$time_start) == base_year
     if (!check_base_year) {
-      print(paste0("Warning: Base year for LocID = ", locationIDs[i], " not equal to ", base_year,". This location is excluded from aggregate."))
+      log_print(paste0("Warning: Base year for LocID = ", locationIDs[i], " not equal to ", base_year,". This location is excluded from aggregate."))
       next()
     }
     
@@ -204,7 +204,7 @@ ccmppWPP_compile <- function(locationIDs, base_year, last_year, intermediate_out
     check_all_years <- all(base_year:last_year %in% unique(ccmpp_output$pop_count_age_sex$time_start))
     if (!check_all_years) {
       missing_years <- c(base_year:last_year)[!(base_year:last_year %in% unique(ccmpp_output$pop_count_age_sex$time_start))]
-      print(paste0("Warning: CCMPP population outputs for LocID = ", locationIDs[i], " missing records for time_start = ", missing_years,". This location is excluded from aggregate."))
+      log_print(paste0("Warning: CCMPP population outputs for LocID = ", locationIDs[i], " missing records for time_start = ", missing_years,". This location is excluded from aggregate."))
       next()
     }
     
